a95e0a4f9be32d14f7a568d13eef31d7748254f2
Jose Ruben Daza Hernandez <rudahee@gmail.com>
2020-06-05 11:14:36 +0200

    Iniciando el archivo readme.md

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Voluntariado
+
+-----
+
+## Indice
+
+1. [Presentacion del proyecto](#presentacion-del-proyecto)
+2. [Requerimientos](#requerimientos)
+3. Diagramas
+   1. Diagrama de clase
+   2. Diagrama de casos de uso
+   3. Diagrama de secuencia
+4. Scripts de creacion sobre la base de datos
+
+## Presentacion del proyecto
+
+Este proyecto esta realizado para las asignaturas de Programacíon y Entornos de Desarrollo para el primer curso de Desarrollo de Aplicaciones Web.
+
+El enunciado del proyecto se puede encontrar [aqui.](https://drive.google.com/file/d/1wxndppVX-6nkW55Zau-0JiMw3Ps6qvAn/view?usp=sharing)
+
+## Requerimientos
+
+- Java 13 o superior
--- src/controlador/ControladorVoluntario.java
+++ src/controlador/ControladorVoluntario.java
@@ -1,40 +1,11 @@
 package controlador;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
-import modelo.Aptitud;
-import modelo.Organizacion;
-import modelo.Proyecto;
-import modelo.Voluntario;
-import modelo.VoluntarioAptitud;
 
 public class ControladorVoluntario {
 	
-	private static StandardServiceRegistry sr = new StandardServiceRegistryBuilder().configure().build();
-	private static SessionFactory sf = new MetadataSources(sr).buildMetadata().buildSessionFactory();
-	private static Session session = sf.openSession();
-	
+
 	public ControladorVoluntario() {
-		session.getTransaction().begin();
-		Voluntario voluntario = new Voluntario("Jose Ruben", "Daza Hernandez");
-		Organizacion ong = new Organizacion("Intermon Oxfand");
-		Proyecto ayudaEnAfrica = new Proyecto("Africa te necesita", "Ayuda en el corazon de la africa mas pobre", "Nueva Guinea", ong); 
-		Aptitud apt = new Aptitud("Organizacion", "Capacidad para organizar eventos", true);
-		VoluntarioAptitud volapt = new VoluntarioAptitud(voluntario, apt);
-		ayudaEnAfrica.addVoluntario(voluntario);
 		
-		session.save(ong);
-		session.save(ayudaEnAfrica);
-		session.save(voluntario);
-		session.save(apt);
-		session.save(volapt);
-		session.getTransaction().commit();
-		session.close();
-		sf.close();
 	}
 	
 }
